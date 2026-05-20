662ca955f9f8211c9db89328c534b801cbd83e59
mehmoodkhan308 <khanzadarajput987@gmail.com>
2026-05-20 20:37:20 +0500

    supabase connect

---
+++ class 1/app.js
@@ -0,0 +1,27 @@
+let { createClient } = supabase;
+
+const PROJECT_URL = 'https://wvgfjuzaqrlqxazgjsxw.supabase.co'
+const PROJECT_KEY = 'sb_publishable_s-aHRNhlx1EKW8z2ArQMrQ_FJ1Tq2Rf'
+
+
+const supabaseClient = createClient(PROJECT_URL, PROJECT_KEY);
+
+console.log(supabaseClient);
+
+const dataCall = async () => {
+
+    const { error } = await supabaseClient
+        .from('countries')
+        .insert({ id: 1, name: 'Mordor' })
+
+}
+
+
+
+
+
+
+
+
+
+
