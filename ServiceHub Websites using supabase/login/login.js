1b57de0297c585902b99c3ab67d0064ffafa56b1
kashifkhanzada65 <muhammadkashifkhanzada110@gmail.com>
2026-06-17 00:08:59 +0500

    practice of supabase using serviceHub websites

---
+++ ServiceHub Websites using supabase/login/login.js
@@ -0,0 +1,3 @@
+import supabaseClient from "../script.js";
+
+
---
+++ ServiceHub Websites using supabase/script.js
@@ -0,0 +1,19 @@
+
+const { createClient } = supabase
+
+const PROJECT_URL = 'https://xpndumycwxitagogupre.supabase.co'
+const PROJECT_KEY = 'sb_publishable_306rNpLlcPwmOMvtFL_ebQ_YLF7YgiA'
+
+const supabaseClient = createClient(PROJECT_URL, PROJECT_KEY)
+
+export default supabaseClient
+
+
+
+
+
+
+
+
+
+
